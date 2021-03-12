3ab0047029084540e61c56f3d1e2676d763cab6e
Adrian Ster <steradrian5@gmail.com>
2021-03-12 21:25:20 +0200

    done

--- src/components/Charts/chartStyles.js
+++ src/components/Charts/chartStyles.js
@@ -4,9 +4,11 @@ export default makeStyles(() => ({
   income: {
     background: 'rgb(255,255,255,0.85)',
     borderBottom: '10px solid rgba(0, 255, 0, 0.5)',
+    margin: '30px 16px',
   },
   expense: {
     background: 'rgb(255,255,255,0.85)',
     borderBottom: '10px solid rgba(255, 0, 0, 0.5)',
+    margin: '30px 16px',
   },
 }));
